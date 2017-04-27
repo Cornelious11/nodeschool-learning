8ebc967996c87df83d89a2b857db04ed73ca4aee
Hilary Southey <hilary@southey.tech>
2017-04-26 22:59:32 -0400

    Finished Exercise 4 of Stream-Adventure

--- sa_ex5.js
+++ sa_ex5.js
@@ -2,10 +2,12 @@ var through = require('through2')
 var stream = through(write, end)
 
 function write(buffer, encoding, next){
-    this.push('I got some data: ' + buffer + '\n')
+    this.push(String(buffer).toUpperCase())
     next()
 }
 
 function end(done){
     done()
 }
+
+process.stdin.pipe(stream).pipe(process.stdout)
